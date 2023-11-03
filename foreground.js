0f48928750ed7a80f953e348e7a7b86d4f69ea28
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2023-11-03 08:23:25 +0200

    adding observable

--- foreground.js
+++ foreground.js
@@ -2,3 +2,21 @@ var element = document.getElementsByClassName("Skip-IntroText")[0];
 if (element) {
   element.click();
 }
+var skippedValue = 0;
+var startingValue = document.getElementsByClassName("min-w-[2em]")[0];
+var endingValue = document.getElementsByClassName("min-w-[2em] mr-[12px]")[0];
+
+var observer = new MutationObserver((mutations) => {
+  for (let mutation of mutations) {
+    // for (let addedNode of mutation.attributeName) {
+    //   console.log("mutation", addedNode);
+    if (mutation.target.id === "theoplayer-container") {
+      if (startingValue < skippedValue) {
+        element.click();
+        skippedValue = startingValue;
+      }
+    }
+    // }
+  }
+});
+observer.observe(document, { childList: true, subtree: true });
